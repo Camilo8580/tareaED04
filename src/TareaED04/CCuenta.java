1f6238791bf9b20ac3d924e0452cc041b2ea447a
Camilo-ED02 <Camilo-ED02@DESKTOP-BV4TCDP>
2022-02-17 18:43:22 +0100

    Commit V2

--- src/TareaED04/CCuenta.java
+++ src/TareaED04/CCuenta.java
@@ -1,91 +1,121 @@
 package TareaED04;
 
-public class CCuenta {
+/**
+ * Clase CCuenta
+ * 
+ * Contiene la información de las cuentas
+ * @author Camilo-ED04
+ * @vesion 1.0
+ */
 
+public class CCuenta {
+    
+    // Metodos publicos
+    
     /**
-     * @return the nombre
+     * Devuelve el nombre de la cuenta
+     * @return nombre cuenta
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Modifica el nombre de la cuenta
+     * @param nombre 
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Devuelve la cuenta
+     * @return cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Modifica la cuenta
+     * @param cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Devuelve el saldo de la cuenta
+     * @return saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Modifica el saldo de la cuenta
+     * @param saldo 
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * Devuelve el tipo de interes
+     * @return tipo de interes
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * Modifica el tipo de interes
+     * @param tipoInterés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
-
+    //Atributos
+    /**
+     * Nombre de la cuenta
+     */
     private String nombre;
+    /**
+     * Numero de la cuenta
+     */
     private String cuenta;
+    /**
+     * Saldo de la cuenta
+     */
     private double saldo;
+    /**
+     * interes de la cuenta
+     */
     private double tipoInterés;
 
     public CCuenta()
     {
     }
-
+    // El valor de la variables introducidas
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    //Nos muestra el estado del saldo
     public double estado()
     {
         return getSaldo();
     }
-
+    // La cantidad de ingreso y sus excepciones
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    // La cantidad a retirar y sus excepciones
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
